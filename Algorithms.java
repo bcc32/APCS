9f82fbc6dbad776a1cf948159967e8d755317fe4
Kuroneko <bcc32@blinkenshell.org>
2012-10-11 10:29:52 -0400

    Use List<T>, not ArrayList<T>

--- Algorithms.java
+++ Algorithms.java
@@ -4,7 +4,7 @@ import java.io.*;
 public class Algorithms
 {
     public static <T extends Comparable<? super T>> void bubbleSort(
-        ArrayList<T> list )
+        List<T> list )
     {
         for ( int i = 1; i < list.size(); i++ )
             for ( int j = 1; j <= list.size() - i; j++ )
@@ -13,7 +13,7 @@ public class Algorithms
     }
 
     public static <T extends Comparable<? super T>> int findMinLoc(
-        ArrayList<T> list )
+        List<T> list )
     {
         int min = 0;
         for ( int i = 1; i < list.size(); i++ )
@@ -23,7 +23,7 @@ public class Algorithms
     }
 
     public static <T extends Comparable<? super T>> int findMinLoc(
-        ArrayList<T> list, int foo )
+        List<T> list, int foo )
     {
         int min = foo;
         for ( int i = foo + 1; i < list.size(); i++ )
@@ -33,7 +33,7 @@ public class Algorithms
     }
 
     public static <T extends Comparable<? super T>> void insert(
-        ArrayList<T> list, T elem )
+        List<T> list, T elem )
     {
         for ( int i = 0; i < list.size(); i++ )
             if ( list.get( i ).compareTo( elem ) >= 0 )
@@ -45,7 +45,7 @@ public class Algorithms
     }
 
     public static <T extends Comparable<? super T>> void insertionSort(
-        ArrayList<T> list )
+        List<T> list )
     {
         for ( int i = 1; i < list.size(); i++ )
             for ( int j = 0; j <= i; j++ )
@@ -82,7 +82,7 @@ public class Algorithms
     }
 
     public static <T extends Comparable<? super T>> void selectionSort(
-        ArrayList<T> list )
+        List<T> list )
     {
         for ( int i = 0; i < list.size(); i++ )
         {
@@ -95,13 +95,13 @@ public class Algorithms
     }
 
     public static <T extends Comparable<? super T>> void sort(
-        ArrayList<T> list )
+        List<T> list )
     {
         mergeSort( list );
     }
 
     public static <T extends Comparable<? super T>> void swap(
-        ArrayList<T> list,
+        List<T> list,
         int i1, int i2 )
     {
         T foo = list.get( i1 );
