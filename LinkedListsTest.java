1f92c87322aecc665eeac5b9471ecac48647f9c3
Kuroneko <bcc32@blinkenshell.org>
2012-12-20 14:05:13 -0500

    spaces in LinkedListsTest

--- LinkedListsTest.java
+++ LinkedListsTest.java
@@ -5,7 +5,7 @@ public class LinkedListsTest
 {
     public static void main( String[] args )
     {
-        System.out.println( "Stack:" );
+        System.out.println( "Stack: " );
         Stack<Integer> stack = new Stack<Integer>();
         stack.push( 2 );
         stack.print();
@@ -35,11 +35,11 @@ public class LinkedListsTest
         queue.pop();
         queue.print();
 
-        System.out.println( "reverseCopy():" );
+        System.out.println( "reverseCopy(): " );
         List<Integer> copy = ListMethods.reverseCopy( stack );
         copy.print();
 
-        System.out.println( "insertInOrder():" );
+        System.out.println( "insertInOrder(): " );
         Random rand = new Random();
         List<Integer> list = new List<Integer>();
         for ( int i = 0; i < 25; i++ )
